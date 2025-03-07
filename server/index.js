5d0147797790a5cd95ee335be0a4ba6856dec4eb
tunahari <tunahari86@gmail.com>
2025-03-07 08:55:51 +0700

    update main.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const path = require('path');
 const port = process.env.PORT || 3000;
-const io = require("socket.io")(3000, {
+const io = require("socket.io")(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
+});
+
+app.use(express.static(path.join(__dirname, '../'))); // Phục vụ các file tĩnh từ thư mục gốc
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html')); // Phục vụ file index.html
+});
+
+http.listen(3000, () => {
+  console.log('Server Socket.IO đang chạy trên cổng 3000');
 });
 
 const arrUserInfo = [];
@@ -28,4 +43,4 @@ io.on("connection", (socket) => {
   });
 });
 
-console.log(`Server Socket.IO đang chạy trên cổng ${port}`);
+// console.log(`Server Socket.IO đang chạy trên cổng ${port}`);
